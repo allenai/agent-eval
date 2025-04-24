466782ba1221ed410bba7e3a90e6eab4d41ef464
Jonathan Bragg <jbragg@allenai.org>
2025-04-24 10:21:37 -0700

    change public interface

--- src/agenteval/__init__.py
+++ src/agenteval/__init__.py
@@ -2,12 +2,12 @@ from importlib.metadata import version as get_version
 
 __version__ = get_version("agent-eval")
 
-from .processor import score_directory
+from .score import process_eval_logs
 from .summary import compute_summary_statistics
 from .upload import upload_folder_to_hf, upload_summary_to_hf
 
 __all__ = [
-    "score_directory",
+    "process_eval_logs",
     "compute_summary_statistics",
     "upload_folder_to_hf",
     "upload_summary_to_hf",
