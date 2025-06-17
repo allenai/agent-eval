c42eea2a38e397246a6227bbe59e39f4f6bc3bed
Regan Huff <reganh@allenai.org>
2025-06-17 15:02:58 -0700

    compress summary before uploading to results

--- src/agenteval/cli.py
+++ src/agenteval/cli.py
@@ -12,7 +12,7 @@ from .config import load_suite_config
 from .models import EvalConfig, EvalResult
 from .score import process_eval_logs
 from .summary import compute_summary_statistics
-from .upload import sanitize_path_component, upload_folder_to_hf, upload_summary_to_hf
+from .upload import sanitize_path_component, upload_folder_to_hf, upload_summary_to_hf, compress_model_usages
 
 EVAL_FILENAME = "agenteval.json"
 
@@ -316,8 +316,10 @@ def publish_command(
     click.echo(f"Uploaded submission logs dir to {logs_url}")
     eval_result.submission.logs_url = logs_url
 
+    compressed_usages_result = compress_model_usages(eval_result)
+
     summary_url = upload_summary_to_hf(
-        hf_api, eval_result, results_repo_id, config_name, eval_result.split, subm_name
+        hf_api, compressed_usages_result, results_repo_id, config_name, eval_result.split, subm_name
     )
     click.echo(f"Uploaded results summary file to {summary_url}")
     eval_result.submission.summary_url = summary_url
